1a7f392ccae3a0ca2285869e95b42604b818a70a
Mitch <mbarron198@gmail.com>
2024-06-27 13:39:27 +1000

    feat: add favourites table

--- supabase/migrations/0_add_core_schema.sql
+++ supabase/migrations/0_add_core_schema.sql
@@ -1,4 +1,5 @@
---auth.users is a supabase's auth table
+-- NOTE: auth.users is a supabase's auth table
+-- 
 CREATE TABLE recipes (
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   user_id UUID REFERENCES auth.users(id),
@@ -9,6 +10,7 @@ CREATE TABLE recipes (
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
+-- 
 CREATE TABLE comments (
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   recipe_id UUID REFERENCES recipes(id),
@@ -17,15 +19,24 @@ CREATE TABLE comments (
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
+-- 
 CREATE TABLE reviews (
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  recipe_id UUID REFERENCES recipes(id),
-  user_id UUID REFERENCES auth.users(id),
+  recipe_id UUID NOT NULL REFERENCES recipes(id) ON DELETE CASCADE,
+  user_id UUID NOT NULL REFERENCES auth.users(id) ON DELETE CASCADE,
   rating INTEGER CHECK (
     rating >= 1
     AND rating <= 5
   ),
   review TEXT,
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  CONSTRAINT unique_recipe_user UNIQUE (recipe_id, user_id)
+);
+-- 
+CREATE TABLE favourites (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  user_id UUID NOT NULL REFERENCES auth.users(id) ON DELETE CASCADE,
+  recipe_id UUID NOT NULL REFERENCES recipes(id) ON DELETE CASCADE,
+  CONSTRAINT unique_recipe_user UNIQUE (recipe_id, user_id)
 );
